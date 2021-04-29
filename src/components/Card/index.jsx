ceb16abe56cb380ba3c145213e115e62df1f3664
iulik1989 <inicula1989@gmail.com>
2021-04-29 11:10:06 +0300

    deleted SASS files, added makeStyle to components

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import './style.css';
 import Button from "../Button";
 import Rating from '@material-ui/lab/Rating';
 import Box from "@material-ui/core/Box";
@@ -7,7 +6,41 @@ import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
     root: {
-        color: "black"
+        color: "black",
+    },
+    cartContainer: {
+        width: "180px",
+        height: "340px",
+        border: "1px solid #E3E5E4",
+        padding: "10px",
+        borderRadius: "8px"
+    },
+    cartImgContainer: {
+        display: "flex",
+        width: "100%",
+        height: "130px",
+    },
+    cartImgContainerImg:{
+        width: "100%",
+        objectFit: "contain"
+
+    },
+    cartName:{
+        display: "flex",
+        justifyContent: "center",
+        fontFamily: "'Roboto', sans-serif"
+    },
+    cartNameP:{
+        fontWeight: "bold",
+        fontSize: "24px",
+        margin: "5px 0",
+        textTransform: "capitalize"
+    },
+    cartPriceP:{
+        color: "#85C645",
+        fontSize: "20px",
+        fontWeight: "bold",
+        margin: "5px 0"
     }
 })
 
@@ -16,20 +49,20 @@ const Card = ({food}) => {
     const classes = useStyles();
 
     return (
-        <div className='cart-container'>
-            <div className="cart-img-container">
-                <img src={food.picture} alt="here should be apple"/>
+        <div className={classes.cartContainer}>
+            <div className={classes.cartImgContainer}>
+                <img src={food.picture} className={classes.cartImgContainerImg} alt="here should be apple"/>
             </div>
-            <div className='cart-name'>
-                <p>{food.name}</p>
+            <div className={classes.cartName}>
+                <p className={classes.cartNameP}>{food.name}</p>
             </div>
             <div>
                 <Box component="fieldset" mb={3} borderColor="transparent">
-                    <Rating className={classes.root} name="read-only" value={rating} max={6} readOnly precision={0.5}/>
+                    <Rating className={classes.root} name="read-only" value={rating} max={6} readOnly precision={0.25}/>
                 </Box>
             </div>
-            <div className="cart-price">
-                <p>${food.price}</p>
+            <div className={classes.cartName}>
+                <p className={classes.cartPriceP}>${food.price}</p>
             </div>
             <Button/>
         </div>
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,40 +1,52 @@
 import React from 'react';
 import food from '../../food'
 import Card from '../Card'
-import './style.css'
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    cardList: {
+        display: "flex",
+        flexWrap: "wrap"
+    },
+    cardContainer: {
+        margin: "5px"
+    }
+});
 
 const foodSorted = food.sort((a, b) => (a.name > b.name) ? 1 : -1);
 const mostPopular = [...food].sort((a, b) => (a.rating < b.rating) ? 1 : -1);
 
 mostPopular.splice(15)
 
-const cardList = foodSorted.map((e) => {
-    return (
-        <div key={e.id} className="cartContainer">
-            <Card food={e}/>
-        </div>
-    );
-})
-
-const mostPopularCardList = mostPopular.map((e) => {
-    return (
-        <div key={e.id} className="cartContainer">
-            <Card food={e}/>
-        </div>
-    );
-})
-
 const Dashboard = () => {
+    const classes = useStyles();
+
+    const cardList = foodSorted.map((e) => {
+        return (
+            <div key={e.id} className={classes.cardContainer}>
+                <Card food={e}/>
+            </div>
+        );
+    });
+
+    const mostPopularCardList = mostPopular.map((e) => {
+        return (
+            <div key={e.id} className={classes.cardContainer}>
+                <Card food={e}/>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h1>Product list</h1>
             <hr/>
-            <div className='cardList'>
+            <div className={classes.cardList}>
                 {cardList}
             </div>
             <h1>Most popular</h1>
             <hr/>
-            <div className='cardList'>
+            <div className={classes.cardList}>
                 {mostPopularCardList}
             </div>
         </div>
