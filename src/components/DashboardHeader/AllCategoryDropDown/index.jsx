a9701617dce2ac05a571e761010c3aac0dc2f849
iulik1989 <inicula1989@gmail.com>
2021-05-03 15:44:58 +0300

    created style file for AllCategory component

--- src/components/DashboardHeader/AllCategoryDropDown/index.jsx
+++ src/components/DashboardHeader/AllCategoryDropDown/index.jsx
@@ -5,60 +5,7 @@ import MenuItem from '@material-ui/core/MenuItem'
 import ListItemText from '@material-ui/core/ListItemText';
 import Box from "@material-ui/core/Box";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
-const useStyles = makeStyles((theme) => ({
-    allCategoryStyle: {
-        backgroundColor: theme.palette.common.white,
-        boxShadow: theme.spacing(0, 0, 0, 0),
-        width: theme.spacing(30),
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        cursor: 'pointer',
-        marginLeft: theme.spacing(3)
-    },
-    navContainer: {
-        display: 'flex',
-        width: '100%',
-        height: theme.spacing(6.5),
-        fontFamily: theme.fontFamily.main,
-        fontSize: theme.spacing(2.5),
-        justifyContent: 'center',
-    },
-    searchContainer: {
-        display: 'flex',
-        alignItems: 'center'
-    },
-    searchInput: {
-        border: 0,
-        outline: 0,
-        width: theme.spacing(50),
-        fontSize: theme.spacing(2)
-    },
-    searchButton: {
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.common.white,
-        height: '100%',
-        width: theme.spacing(16),
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    iconBetween: {
-        display: 'flex',
-        alignItems: 'center',
-        color: '#CECECE',
-        marginLeft: theme.spacing(3),
-        marginRight: theme.spacing(3),
-        fontSize: theme.spacing(2)
-    },
-    categoryAndSearchContainer: {
-        border: '1px solid #CECECE',
-        display: "flex",
-
-    }
-}))
+import useStyles from "./style";
 
 const StyledMenu = withStyles({
     paper: {
@@ -159,6 +106,3 @@ const AllCategoryDropDown = () => {
 }
 
 export default AllCategoryDropDown;
-
-
-
