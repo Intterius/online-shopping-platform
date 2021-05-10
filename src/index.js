e92c40e8f76641e7c702ada4f0dead322e54a208
icuzmici <intel66666666@gmail.com>
2021-05-10 21:15:32 +0300

    Connected to backend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import 'fontsource-roboto';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {ThemeProvider} from "@material-ui/core";
-import theme from "./Theme";
+import { ThemeProvider } from '@material-ui/core';
+import theme from './Theme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { tokenReducer } from './redux/reducers/tokenReducer';
+
+const store = createStore(tokenReducer);
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <App/>
-    </ThemeProvider>,
-    document.getElementById('root'));
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,13 @@
 import Dashboard from '../../components/Dashboard';
-import DashboardHeader from "../../components/DashboardHeader";
+import DashboardHeader from '../../components/DashboardHeader';
 
 const Home = () => {
-    return (
-        <>
-            <DashboardHeader />
-            <Dashboard/>
-        </>
-    );
+  return (
+    <>
+      <DashboardHeader />
+      <Dashboard />
+    </>
+  );
 };
 
 export default Home;
